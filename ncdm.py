b1bf8c84fa5e4b25cc37271682570d1e3ff9632a
Jens Stuecker <jstuecker@dipc.org>
2021-09-20 22:48:08 +0200

    Cleaned up the code, added documentation

--- ncdm.py
+++ ncdm.py
@@ -1,14 +1,27 @@
+# This file offers functions to estimate the relative surpression in the
+# halo and subhalo mass functions of non cold dark matter models with
+# respect to a CDM reference case.
+#
+# For detailed explanations, please see the publication Stücker et al (2021)
+# Code by Jens Stuecker
+
 import numpy as np
 from scipy.optimize import root
 
-# For detailed explanations, please see the publication Stücker et al (2021)
-# ToDo: - add some documentation
-#       - add function to fit power spectrum surpressions
-#       - add a notebook with examples
 
 #========================== Some Helper Functions ===========================#
 
 def get_mscales(beta, mhm=1., mode="halo"):
+    """The scales where the mass function is surpressed by 20%, 50% and 80%
+    
+    beta : The slope parameter describing the power-spectrum surpression
+    mhm :  The half-mode mass (describing the scale of the cut-off). Use
+          mhm=1. to get outputs in units of the half-mode mass.
+    mode : can be 'halo' or 'satellite'
+          
+    returns: np.array with M20, M50, M80 as elements. Output masses are in the 
+             same units as the input parameter mhm.
+    """
     if mode=="halo":
         mu = np.array((0.2651, 1.638, 16.51))
         nu = np.array((0.3656, -0.0994, -0.9466))
@@ -16,17 +29,32 @@ def get_mscales(beta, mhm=1., mode="halo"):
         mu = np.array((0.1259, 1.134, 20.52))
         nu = np.array((0.4963, -0.0110, -1.1231))
     else:
-        raise ValueError("Unknown mode %s, please use either mode='halo' or mode='satellite'" % mode)
+        raise ValueError("Unknown mode %s, please use either mode='halo' "
+                         "or mode='satellite'" % mode)
         
     return mhm*mu*beta**nu
 
 def mass_function_abc(m, a, b, c):
+    """The parametrized surpression function of the halo mass function"""
     return (1 + (a / m)**b )**c
 
 def supp_mscale(a, b, c, frac=0.5):
-    return a / (frac**(1./c) - 1)**(1./b)
+    """The mass scale where the surpression reaches a given value, given a,b,c
+    
+    a, b, c : parameters (a >= 0, b >= 0, c <= 0)
+
+    frac : desired surpression factor
+    
+    returns : the mass M where f(M)=frac, units are as the parameter "a"
+    """
+    if (a < 0) | (b < 0) | (c > 0): 
+        # This is just here, to tell the fitting function when it goes out of bounds
+        return np.nan
+    
+    return a / (frac**(1./c) - 1.)**(1./b)
 
 def mscales_to_abc(m20, m50, m80):
+    """Uniquely maps the parameters m20, m50, m80 to a, b, c"""
     def equations(par):
         a, b, c =  10**par[0], par[1], par[2]
 
@@ -35,7 +63,10 @@ def mscales_to_abc(m20, m50, m80):
         eq = np.log10(ms) - np.log10([m20, m50, m80])
 
         return eq
-
+    
+    def fitfunc(frac, a, b, c):
+        return supp_mscale(a, b, -c, frac=frac)
+    
     p0 = (np.log10(m50), 2., -1.)
     res =  root(equations, p0, method="lm")
 
@@ -44,6 +75,16 @@ def mscales_to_abc(m20, m50, m80):
 #====================== Main Functions for use cases ========================#
 
 def mass_function_beta_mhm(m, beta=2., mhm=1., mode="halo"):
+    """Evaluates the mass function of a ncdm model
+    
+    m : the masses where to evaluate the mass function, same units as mhm
+    beta : the slope of the primordial power spectrum cutoff
+    mhm : the half mode mass -- describing the scale of the cutoff
+    mode : can be "halo" or "satellite"
+    
+    returns : the relative suppression of the ncdm to the cdm mass function
+    """
+    
     m20,m50,m80 = get_mscales(beta=beta, mhm=mhm, mode=mode)
     a,b,c = mscales_to_abc(m20,m50,m80)
     
---
+++ scales.py
@@ -0,0 +1,84 @@
+# This file offers some helper functions to handle the power spectra
+# of a non cold dark matter universe and the length and mass scales
+#
+# For detailed explanations, please see the publication Stücker et al (2021)
+# Code by Jens Stuecker
+
+import numpy as np
+
+def transfer_ncdm(k, alpha, beta, gamma=5.):
+    """The non-cold dark amtter transfer function as in Murgia(2018) and Stucker (2021)"""
+    return (1. + (alpha*k)**beta)**(-gamma)
+
+def alpha_wdm(mx=1., mode="schneider", omega_x=0.27, h=0.7, gx=1.5, verbose=False):
+    """Parameter of the power spectrum suppression for a given WDM thermal relic mass
+    
+    Uses either the fit from Schneider (2013) or Bode (2001)
+
+    mx : thermal relic mass in eV
+    mode : can be mode="schneider" (recommended) or "bode_varynu" or "bode_fixednu"
+    omega_x : dark matter density parameter
+    h : redued hubble parameter
+    gx : degeneracy factor (only used by mode = "bode_varynu")
+    omega_m : matter density parameter
+    
+    returns : the parameter alpha in units of Mpc/h
+    """
+    #alpha is in h/mpc
+    if mode == "bode_fixednu": # next to (A7) of the bode paper, this is what music was trying to use, I think
+        return 0.05 * (omega_x/0.4)**0.15 * (h/0.65)**1.3 * (mx/1.)**-1.15 * (1.5/gx)**0.29
+    elif mode == "bode_varynu": # This is the fitting formula in bode when allowing nu to vary
+        return 0.048 * (omega_x/0.4)**0.15 * (h/0.65)**1.3 * (mx/1.)**-1.15 * (1.5/gx)**0.29
+    elif (mode == "murgia") | (mode == "schneider"): # This is the formula used in the murgia paper arxiv:1704.07838, it originates from the schneider paper
+        return 0.049 * (omega_x/0.25)**0.11 * (h/0.7)**1.22  * (mx/1.)**-1.11
+    else:
+        raise ValueError("Unknown mode %s" % mode)
+
+def alpha_beta_gamma_3_to_2_par(alpha, beta, gamma, gammamap=-5., kfit=None):
+    """Maps a 3 parameter transfer function to a two parameter one, fixing gamma
+    
+    alpha, beta, gamma : 3 parameter description
+    gammamap : fixed gamma value (in Stucker (2021) it is assumed 5)
+    kfit : (optional) a set of kvalues where to fit the approximation
+    
+    returns : (alphamap, betamap)  -- the two parameter approximaton
+    """
+    
+    from scipy.optimize import curve_fit
+
+    if kfit is None:
+        kfit = np.logspace(np.log10(1e-4/alpha), np.log10(1e4/alpha), 300)
+    Tfit = transfer_ncdm(kfit, alpha, beta, gamma)
+    
+    (alphamap, betamap), cov = curve_fit(transfer_ncdm, kfit, Tfit, p0=(alpha, beta), bounds=(0, np.inf))
+    
+    return alphamap, betamap, gammamap
+
+def k_half_mode(alpha, beta, gamma):
+    """finds the wavenumber where the transfer function is surpressed by 1/2"""
+    fraction = 0.5
+    return 1./alpha * (fraction**(-1./gamma) - 1. )**(1./beta)
+
+def half_mode_volume(alpha, beta, gamma):
+    """The Lagrangian volume that is associated with the halfmode scale"""
+    khm = k_half_mode(alpha, beta, gamma)
+    lam = (2.*np.pi)/khm
+    
+    return 4.*np.pi/3. * (lam/2.)**3
+
+def background_density(omega_m=0.31):
+    """The mean matter density of the universe in (Msol/h)/(Mpc /h)**3"""
+    parsec = 3.085677581491367e+16    # one parsec in SI units
+    G = 6.6743e-11                    # Gravitational constant in SI units
+    Msol = 1.98847e30                 # Solar mass in SI units
+    
+    H0_SI = 100e3 / (1e6*parsec)      # 100 km/s / Mpc in SI units
+    rhocrit_SIh2 = 3.*H0_SI**2 / (8.*np.pi*G)
+    
+    rhocrit = rhocrit_SIh2 / Msol * (1e6*parsec)**3
+    
+    return omega_m * rhocrit
+
+def half_mode_mass(alpha, beta, gamma, omgea_m=0.31):
+    """Calculates the half mode mass in units of Msol/h"""
+    return half_mode_volume(alpha, beta, gamma) * background_density(omega_m=omgea_m)
